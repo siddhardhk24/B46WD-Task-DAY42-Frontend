ed6f35d4ca00c854ef88739efac92b34e1d4fe95
siddhardhk24 <anandhsidhu541@gmail.com>
2023-10-20 09:05:32 +0530

    frontend code

---
+++ src/apis/books.js
@@ -0,0 +1,44 @@
+export function getBooksAPI() {
+    return fetch("https://day-42-back.onrender.com/books")
+      .then((res) => res.json())
+      .then((data) => data);
+  }
+  
+  export function addBookAPI(book) {
+    try {
+      return fetch("https://day-42-back.onrender.com/books", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+      })
+        .then((res) => res.json())
+        .then((data) => data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  export function updateBookAPI(book) {
+    return fetch("https://day-42-back.onrender.com/books/" + book._id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+  }
+  
+  export function deleteBookAPI(id) {
+    return fetch(`https://day-42-back.onrender.com/books/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+  }
